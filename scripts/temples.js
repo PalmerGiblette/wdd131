8061fc760da66ed77e18d9ee912ed0c2e0f2192d
PalmerGiblette <palmerdavidg@yahoo.com>
2024-10-15 16:42:01 -0600

    Updated and added hamburger menu, with extra color

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,13 @@
 let today = new Date();
 document.getElementById("copy").innerHTML = today.getFullYear();
 document.querySelector("#lastmod").innerHTML = document.lastModified;
+
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
